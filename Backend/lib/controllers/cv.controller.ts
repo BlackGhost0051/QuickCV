4b781703ad8807a35157bd061fa8fe13d0df24fe
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-14 22:57:02 +0100

    Add puppeteer

--- Backend/lib/controllers/cv.controller.ts
+++ Backend/lib/controllers/cv.controller.ts
@@ -1,5 +1,6 @@
 import Controller from '../interfaces/controller.interface';
 import {Router, Request, Response, NextFunction} from "express";
+import puppeteer from "puppeteer";
 
 class CvController implements Controller{
     public path = '/cv';
@@ -11,12 +12,17 @@ class CvController implements Controller{
     }
 
     private initializeRoutes() {
-        this.router.get(`${this.path}/generate`, this.generate);
+        this.router.get(`${this.path}/generate-pdf`, this.generate_pdf);
     }
 
 
-    private generate = async (request: Request, response: Response) => {
-        return response.status(200).json({ message: "TEST" });
+    private generate_pdf = async (request: Request, response: Response) => {
+        try {
+
+        } catch (error) {
+            console.error("Error generating PDF:", error);
+            response.status(500).json({ error: "Failed to generate PDF" });
+        }
     };
 }
 
