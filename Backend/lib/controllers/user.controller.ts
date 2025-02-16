87227ddfc024e52c029f28b608bdabb7a9e29a79
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-16 15:52:06 +0100

    Connect user.controller.ts to index.ts

--- Backend/lib/controllers/user.controller.ts
+++ Backend/lib/controllers/user.controller.ts
@@ -29,3 +29,5 @@ class UserController implements Controller{
 
     }
 }
+
+export default UserController;
--- Backend/lib/index.ts
+++ Backend/lib/index.ts
@@ -1,8 +1,10 @@
 import App from './app'
 import CvController from "./controllers/cv.controller";
+import UserController from "./controllers/user.controller";
 
 const app: App = new App([
-    new CvController()
+    new CvController(),
+    new UserController()
 ]);
 
 app.listen();
