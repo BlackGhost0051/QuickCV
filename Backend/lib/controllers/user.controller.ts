1b711d25b01731b7fd6e660d7f83ce00ae5990de
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-16 15:50:41 +0100

    Add user.controller.ts

---
+++ Backend/lib/controllers/user.controller.ts
@@ -0,0 +1,31 @@
+import Controller from "../interfaces/controller.interface";
+import {Router} from "express";
+
+
+class UserController implements Controller{
+    public path = '/user';
+    public router = Router();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes() {
+        this.router.post(`${this.path}/auth`, this.authenticate);
+        this.router.post(`${this.path}/register`, this.register);
+
+        this.router.patch(`${this.path}/change-password`, this.changePassword);
+    }
+
+    private authenticate(){
+
+    }
+
+    private register(){
+
+    }
+
+    private changePassword(){
+
+    }
+}
