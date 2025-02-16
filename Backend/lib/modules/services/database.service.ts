cf997317d046907a3cc5b792fe7483590adc8cad
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-16 16:05:04 +0100

    Mod database.service.ts

--- Backend/lib/modules/services/database.service.ts
+++ Backend/lib/modules/services/database.service.ts
@@ -1,12 +1,51 @@
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import { open, Database } from "sqlite";
 
 
 class DatabaseService {
+    private db: Database | null = null;
 
-    constructor() {
+    constructor() {}
+
+    async connect(){
+        try{
+            this.db = await open({
+                filename: './test-database.db',
+                driver: sqlite3.Database
+            });
+
+            await this.db.exec(`
+                CREATE TABLE IF NOT EXISTS users (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  login TEXT NOT NULL,
+                  password TEXT NOT NULL,
+                  isAdmin INTEGER DEFAULT 0
+                );
+            `);
+        } catch (error){
+            console.error("Error connecting to the database:", error);
+        }
+    }
+
+
+
+
+    async addUser(){
 
     }
+
+    async getUserByLogin(login: string){
+
+    }
+
+    async deleteUser(login: string){
+
+    }
+
+    async changePassword(login: string){
+
+    }
+
 }
 
 export default DatabaseService;
