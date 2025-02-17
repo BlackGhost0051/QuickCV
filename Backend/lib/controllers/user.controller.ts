bf76d5e612e8edff7eaf0d80716ebc31a9da2b9d
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-17 16:19:31 +0100

    Add authenticate

--- Backend/lib/controllers/user.controller.ts
+++ Backend/lib/controllers/user.controller.ts
@@ -1,12 +1,13 @@
 import Controller from "../interfaces/controller.interface";
 import {Request, Response, Router} from "express";
 import DatabaseService from "../modules/services/database.service";
-
+import PasswordService from "../modules/services/password.service";
 
 class UserController implements Controller{
     public path = '/user';
     public router = Router();
     private dbService: DatabaseService;
+    private passwordService: PasswordService;
 
     constructor() {
         this.dbService = new DatabaseService();
@@ -14,15 +15,38 @@ class UserController implements Controller{
     }
 
     private initializeRoutes() {
-        this.router.post(`${this.path}/auth`, this.authenticate);
+        this.router.post(`${this.path}/auth`, this.authenticate.bind(this));
         this.router.post(`${this.path}/register`, this.register.bind(this));
 
         this.router.patch(`${this.path}/change-password`, this.changePassword);
     }
 
 
-    private authenticate(){
+    private async authenticate(request: Request, response: Response){
+        const { login, password } = request.body;
 
+        if(!login || !password){
+            return response.status(400).send("Login and password are required.");
+        }
+
+
+        try{
+            const user = await this.dbService.getUserByLogin(login);
+
+            if (!user) {
+                return response.status(404).send("User not found.");
+            }
+
+            const passwordMatch = await this.passwordService.comparePassword(password, user.password);
+
+            if(!passwordMatch){
+                return response.status(401).send("Invalid credentials.");
+            }
+
+            response.status(200).send("Authentication successful!");
+        } catch (error){
+            response.status(500).send(`Error during authentication: ${error}`);
+        }
     }
 
     private async register(request: Request, response: Response){
