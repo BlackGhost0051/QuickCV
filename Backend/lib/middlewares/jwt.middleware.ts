4a5ebf61a8bf325725303f763b31268795d6ad9c
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-21 18:13:34 +0100

    Mod jwt.middleware.ts

--- Backend/lib/middlewares/jwt.middleware.ts
+++ Backend/lib/middlewares/jwt.middleware.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction} from "express";
 
 const verifyToken = (request: Request, response: Response, next: NextFunction)=> {
+    let token = request.headers['x-auth-token'] || request.headers['authorization'];
 
+    if (!token) {
+        token = request.cookies.token;
+    }
 }
 
 export default verifyToken;
