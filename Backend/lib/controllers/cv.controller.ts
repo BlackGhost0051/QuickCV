11b410b3ad9ae62f622d111fcd52d54a9fdc4fd8
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-22 14:26:04 +0100

    Mod jwt.middleware.ts

--- Backend/lib/controllers/cv.controller.ts
+++ Backend/lib/controllers/cv.controller.ts
@@ -1,6 +1,7 @@
 import Controller from '../interfaces/controller.interface';
 import {Router, Request, Response, NextFunction} from "express";
 import puppeteer from "puppeteer";
+import jwtMiddleware from "../middlewares/jwt.middleware";
 
 class CvController implements Controller{
     public path = '/cv';
@@ -12,13 +13,13 @@ class CvController implements Controller{
     }
 
     private initializeRoutes() {
-        this.router.get(`${this.path}/generate-pdf`, this.generate_pdf);
+        this.router.get(`${this.path}/generate-pdf`, jwtMiddleware ,this.generate_pdf);
     }
 
 
     private generate_pdf = async (request: Request, response: Response) => {
         try {
-
+            response.status(200).json({ status: "OK"});
         } catch (error) {
             console.error("Error generating PDF:", error);
             response.status(500).json({ error: "Failed to generate PDF" });
--- Backend/lib/middlewares/jwt.middleware.ts
+++ Backend/lib/middlewares/jwt.middleware.ts
@@ -1,10 +1,28 @@
 import { Request, Response, NextFunction} from "express";
+import jwt from "jsonwebtoken";
 
 const verifyToken = (request: Request, response: Response, next: NextFunction)=> {
     let token = request.headers['x-auth-token'] || request.headers['authorization'];
 
     if (!token) {
-        token = request.cookies.token;
+        token = request.cookies?.token;
+    }
+
+    if(token && typeof token === 'string'){
+        if (token.startsWith('Bearer ')){
+            token = token.slice(7, token.length);
+        }
+
+        try{
+            console.log("TOKEN === ", token);
+
+            next();
+        } catch (ex){
+            return response.status(400).send('Invalid token.');
+        }
+
+    } else {
+        return response.status(401).send('Access denied. No token provided.');
     }
 }
 
