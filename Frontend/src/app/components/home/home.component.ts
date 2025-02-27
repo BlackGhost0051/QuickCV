d7e67761bc9ca9f4c8f8e686d0374ae6b742943f
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-27 18:04:57 +0100

    Mod home component

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {FooterComponent} from '../footer/footer.component';
+import {HomeGenerateCvComponent} from '../home-generate-cv/home-generate-cv.component';
+import {HomeHeaderComponent} from '../home-header/home-header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent, HomeGenerateCvComponent, HomeHeaderComponent],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
