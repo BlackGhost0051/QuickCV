1b3b72d3128b4cf7e4edab0d583f53db2ed48fb1
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-28 23:14:40 +0100

    Add link

--- Frontend/src/app/components/home-generate-cv/home-generate-cv.component.ts
+++ Frontend/src/app/components/home-generate-cv/home-generate-cv.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-generate-cv',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home-generate-cv.component.html',
   standalone: true,
   styleUrl: './home-generate-cv.component.css'
