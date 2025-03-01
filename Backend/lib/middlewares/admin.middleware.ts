13b6b8637061dee82049185f81ccfd15d9c2733f
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-01 20:34:16 +0100

    Mod admin.middleware.ts

--- Backend/lib/middlewares/admin.middleware.ts
+++ Backend/lib/middlewares/admin.middleware.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import JwtService from "../modules/services/jwt.service";
+import DatabaseService from "../modules/services/database.service";
 
+const jwtService = new JwtService();
+const databaseService = new DatabaseService();
 
-const verifyAdmin = ( request: Request, response: Response, next: NextFunction) => {
+const verifyAdmin = async ( request: Request, response: Response, next: NextFunction) => {
     let token = request.headers['x-auth-token'] || request.headers['authorization'];
 
     if (!token) {
@@ -15,7 +19,20 @@ const verifyAdmin = ( request: Request, response: Response, next: NextFunction) 
 
         try{
             console.log("ADMIN ", token);
-            // need verify token and user isAdmin !!!
+
+            const decoded = jwtService.verifyToken(token);
+            const login: string = decoded.login;
+
+
+            await databaseService.connect();
+            const isAdmin = await databaseService.isAdmin(login);
+
+
+            if(!isAdmin){
+                return response.status(403).json({ error: "Access denied. Admins only." });
+            }
+
+
             next();
         } catch (ex){
             return response.status(400).send('Invalid token.');
