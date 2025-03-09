a97280c4a7bd3da4da3e92295eb9e7b2ef2e56cb
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-09 06:41:16 +0100

    Add email input

--- Frontend/src/app/components/signup/signup.component.ts
+++ Frontend/src/app/components/signup/signup.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './signup.component.html',
   standalone: true,
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
+
+  onSubmit(){
+    console.log(this.username, this.email, this.password, this.confirmPassword);
+  }
 }
