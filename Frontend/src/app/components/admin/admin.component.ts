b1050898b7b6802d1697463ed0167189bb159ffd
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-13 00:37:03 +0100

    Add users list

--- Frontend/src/app/components/admin/admin.component.ts
+++ Frontend/src/app/components/admin/admin.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './admin.component.html',
   standalone: true,
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  users: any[] = [];
 
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.adminService.getAllUsers().subscribe((data: any) => {
+      this.users = data;
+    });
+  }
+
+  deleteUser(login: string) {
+    this.adminService.deleteUser(login).subscribe(() => {
+      this.loadUsers();
+    });
+  }
 }
