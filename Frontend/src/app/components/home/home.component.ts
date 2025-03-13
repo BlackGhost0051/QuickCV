3f4c7da3910ec20149208b10a503839f69e153b1
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-13 21:22:31 +0100

    Add components

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import {FooterComponent} from '../footer/footer.component';
 import {HomeGenerateCvComponent} from '../home-generate-cv/home-generate-cv.component';
 import {HomeHeaderComponent} from '../home-header/home-header.component';
+import {HomeWelcomeComponent} from '../home-welcome/home-welcome.component';
+import {HomeTestimonialsComponent} from '../home-testimonials/home-testimonials.component';
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, HomeGenerateCvComponent, HomeHeaderComponent],
+  imports: [FooterComponent, HomeGenerateCvComponent, HomeHeaderComponent, HomeWelcomeComponent, HomeTestimonialsComponent],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
