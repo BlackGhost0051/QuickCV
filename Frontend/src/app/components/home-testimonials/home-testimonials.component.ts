2846c1f20d4361a2f8ff170d6108b2e1f01abee4
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-13 21:21:24 +0100

    Add testimonials

---
+++ Frontend/src/app/components/home-testimonials/home-testimonials.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-testimonials',
+  imports: [],
+  templateUrl: './home-testimonials.component.html',
+  standalone: true,
+  styleUrl: './home-testimonials.component.css'
+})
+export class HomeTestimonialsComponent {
+
+}
