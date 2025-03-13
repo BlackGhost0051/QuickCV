0d593c5518d0095d6a57d8c4b646700992d57e87
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-13 21:22:15 +0100

    Add welcome

---
+++ Frontend/src/app/components/home-welcome/home-welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-welcome',
+  imports: [],
+  templateUrl: './home-welcome.component.html',
+  standalone: true,
+  styleUrl: './home-welcome.component.css'
+})
+export class HomeWelcomeComponent {
+
+}
