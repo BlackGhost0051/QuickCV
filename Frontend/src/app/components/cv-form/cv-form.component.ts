3710d1849f6389201d62d2f5262b02abb3066656
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-18 23:12:01 +0100

    Add preview

--- Frontend/src/app/components/cv-form/cv-form.component.ts
+++ Frontend/src/app/components/cv-form/cv-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-cv-form',
@@ -10,13 +12,52 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class CvFormComponent implements OnInit{
   cvId!: number;
+  url: string = "http://localhost:3100/api";
+  formContent: SafeHtml = '';
 
-  constructor(private route: ActivatedRoute) {
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
     this.cvId = Number(this.route.snapshot.paramMap.get('id'));
     console.log("Loaded CV Form with ID:", this.cvId);
+
+
+
+    let formData: any = {
+      "userData": {
+        "name": "John Doe",
+        "jobTitle": "Software Engineer",
+        "phone": "+88 888 888 888",
+        "email": "john.doe@example.com",
+        "github": "https://github.com/johndoe",
+        "about": "I am a third-year Computer Science student with a passion for web development.",
+        "education": {
+          "institution": "State University",
+          "degree": "Bachelor of Science in Computer Science",
+          "status": "Currently student"
+        },
+        "skills": {
+          "languages": ["HTML", "CSS", "JavaScript", "Python"],
+          "frameworks": ["Express", "Node.js", "Flask"]
+        },
+        "achievements": "Won first place in a local hackathon."
+      },
+      "formId": this.cvId
+    }
+
+
+
+    this.http.post<any>(`${this.url}/cv/get_form`, formData).subscribe(
+      (response) => {
+        console.log("Received form data:", response);
+        this.formContent = this.sanitizer.bypassSecurityTrustHtml(response);
+      },
+      (error) => {
+        console.error("Error fetching form data:", error);
+      }
+    );
   }
 
 }
