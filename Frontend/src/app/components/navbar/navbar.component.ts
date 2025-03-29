4bdb05ee5bbab006592dc50d218c21185832daba
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-03-29 18:31:38 +0100

    Mod links

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   imports: [
     RouterLink
   ],
+  providers: [AuthService],
   templateUrl: './navbar.component.html',
   standalone: true,
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(public authService: AuthService) {
+  }
+
+  logout(){
+    this.authService.logout();
+  }
+
 }
