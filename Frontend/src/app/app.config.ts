213a5a12c6e7d5ace4c1eb7141c31e6a892cc39f
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-04-06 17:59:49 +0200

    Add provideAnimations

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -7,11 +7,14 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { authInterceptor } from './services/auth/auth.interceptor';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
   ]
 };
